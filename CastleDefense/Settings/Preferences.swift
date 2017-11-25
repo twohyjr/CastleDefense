553f5c54531c4f8ddbf8327593c55b120c571536
Rick Twohy <twohyjr@gmail.com>
2017-11-24 23:13:46 -0600

    Created Preferences

--- CastleDefense/Settings/Preferences.swift
+++ CastleDefense/Settings/Preferences.swift
@@ -2,6 +2,9 @@ import MetalKit
 
 class Preferences{
     
-    static var clearColor: MTLClearColor = MTLClearColor(red: 0.25, green: 0.35, blue: 0.25, alpha: 1.0)
+    static var clearColor: MTLClearColor = MTLClearColor(red: 0.132552, green: 0.366699, blue: 0.410022, alpha: 1.0)
     
+    public static func updateClearColor(color: NSColor){
+        Preferences.clearColor = MTLClearColor(red: Double(color.redComponent), green: Double(color.greenComponent), blue: Double(color.blueComponent), alpha: Double(color.alphaComponent))
+    }
 }
